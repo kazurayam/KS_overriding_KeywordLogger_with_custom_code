bdcac71368bda852f0b6835f69d5824fec9ad8e5
kazurayam <kazuaki.urayama@gmail.com>
2025-02-26 18:35:32 +0900

    renamed KeywordLogger.groovy -> KeywordLogger.java; add TS1

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ecdd8146-9075-435e-9766-1525013870ce</testSuiteGuid>
+   <testCaseLink>
+      <guid>2e1a8176-7155-4e0a-b690-397941520109</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
